eae8b5873e020dad09c942cbe75e7933cde622bc
Aleksandar Kurbalija <aleksandar.kurbalija@stud.fh-campuswien.ac.at>
2023-04-30 16:38:20 +0200

    Added: - dependencies for: "ORMLite" & "H2 Database" - sets a require for ORMLite in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module at.ac.fhcampuswien.fhmdb {
     requires okhttp3;
     requires com.google.gson;
 
+    requires ormlite.jdbc;
+
     opens at.ac.fhcampuswien.fhmdb.models to com.google.gson;
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
